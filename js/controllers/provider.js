eb4cbc32d590043296da3d1923ea58aeb2b7f700
abdelazizFounas <founasabd@gmail.com>
2017-03-01 18:01:47 +0100

    New features added to the provider view.

--- js/controllers/provider.js
+++ js/controllers/provider.js
@@ -4,6 +4,101 @@
 
 cocaas_app = angular.module('cocaasapp');
 
-cocaas_app.controller("controllerProvider", function ($scope) {
+cocaas_app.controller("controllerProvider", function ($scope, $mdDialog) {
   $scope.provider();
+
+  $scope.insertDockerMachine = function(ev) {
+    $mdDialog.show({
+      controller: InsertDockerMachineController,
+      scope: this,
+      preserveScope: true,
+      templateUrl: 'insert-docker-machine.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function InsertDockerMachineController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
+
+  $scope.deleteDockerMachine = function(ev) {
+    $mdDialog.show({
+      controller: DeleteDockerMachineController,
+      scope: this,
+      preserveScope: true,
+      templateUrl: 'delete-docker-machine.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function DeleteDockerMachineController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
+
+  $scope.usersContainers = function(ev) {
+    $mdDialog.show({
+      controller: UsersContainersController,
+      scope: this,
+      preserveScope: true,
+      templateUrl: 'users-containers.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function UsersContainersController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
+
+  $scope.availableImages = function(ev) {
+    $mdDialog.show({
+      controller: AvailableImagesController,
+      scope: this,
+      preserveScope: true,
+      templateUrl: 'available-images.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function AvailableImagesController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
+
+  $scope.nodes = function(ev) {
+    $mdDialog.show({
+      controller: NodesController,
+      scope: this,
+      preserveScope: true,
+      templateUrl: 'nodes.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function NodesController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
 });
